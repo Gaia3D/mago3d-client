331bf303cbd91784604c9752c95981e5cdc3f987
sungjun-gaia3d <sjcho@gaia3d.com>
2024-07-30 13:58:53 +0900

    feat: object 클릭 이벤트 도구 1개만 활성화하는 기능

--- front/packages/user/src/components/ObjectToolbox.tsx
+++ front/packages/user/src/components/ObjectToolbox.tsx
@@ -1,11 +1,33 @@
-import React, { useEffect, useRef } from 'react';
+import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import * as Cesium from 'cesium';
 import { useRecoilState } from "recoil";
 import { OptionsState } from "@/recoils/Tool.ts";
 import { useGlobeController } from "@/components/providers/GlobeControllerProvider.tsx";
 import { useObjectTool } from "@/hooks/useObjectTool.tsx";
 
-export const ObjectToolbox = () => {
+interface MapTool {
+    toolBoxIndex: number;
+    className: string;
+    label: string;
+    active?: boolean;
+    toggle?: boolean;
+    group?: string;
+    onClick: () => void;
+}
+
+type ToolClicked = (tool: MapTool) => void;
+
+const CLICK_EVENT_GROUP = "clickEvent";
+
+const ToolButton = ({ tool, handleClick }: { tool: MapTool, handleClick: (tool: MapTool) => void }) => (
+    <button
+        type="button"
+        className={`${tool.className} ${ (tool.active && tool.toggle) ? 'active' : ''}`}
+        onClick={() => handleClick(tool)}
+    >{tool.label}</button>
+);
+
+export const ObjectToolbox = ({onToolClick}: { onToolClick: ToolClicked }) => {
     const [options] = useRecoilState(OptionsState);
     const SIDE_MENU_WIDTH = 350;
 
@@ -14,6 +36,54 @@ export const ObjectToolbox = () => {
     const { viewer } = globeController;
 
     const { toggleTranslation, toggleRotation, toggleScaling, copyObject, removeObject, objectAddFloor, objectRemoveFloor, toggleColoring, objectColoring } = useObjectTool();
+
+    const initialTools: MapTool[] = useMemo(() => [
+        { toolBoxIndex: 1, className: "translation", label: "이동", group: CLICK_EVENT_GROUP, toggle: true, onClick: toggleTranslation },
+        { toolBoxIndex: 1, className: "rotation", label: "회전", group: CLICK_EVENT_GROUP, toggle: true, onClick: toggleRotation },
+        { toolBoxIndex: 1, className: "scaling", label: "크기", group: CLICK_EVENT_GROUP, toggle: true, onClick: toggleScaling },
+        { toolBoxIndex: 1, className: "copy", label: "복사", group: CLICK_EVENT_GROUP, toggle: true, onClick: copyObject },
+        { toolBoxIndex: 1, className: "remove", label: "삭제", onClick: removeObject },
+        { toolBoxIndex: 1, className: "add-floor", label: "층+", onClick: objectAddFloor },
+        { toolBoxIndex: 1, className: "remove-floor", label: "층-", onClick: objectRemoveFloor },
+        { toolBoxIndex: 1, className: "coloring", label: "색상", group: CLICK_EVENT_GROUP, toggle: true, onClick: toggleColoring },
+    ], [toggleTranslation, toggleRotation, toggleScaling, copyObject, removeObject, objectAddFloor, objectRemoveFloor, toggleColoring ]);
+
+
+    const [tools, setTools] = useState<MapTool[]>(initialTools);
+
+    const isPrevTool = (tool: MapTool, clickedTool: MapTool) =>
+        tool.group !== undefined && tool.className !== clickedTool.className;
+
+    const updateToolActiveState = useCallback((clickedTool: MapTool) => {
+        console.log(clickedTool.className)
+        setTools(prevState =>
+            prevState.map(tool =>
+                isPrevTool(tool, clickedTool)
+                    ? { ...tool, active: false }
+                    : tool.className === clickedTool.className
+                        ? { ...tool, active: !tool.active }
+                        : tool
+            )
+        );
+    }, []);
+
+    const deactivatePreviousActiveTool = useCallback((clickedTool: MapTool) => {
+        setTools(prevState =>
+            prevState.map(tool => {
+                if (isPrevTool(tool, clickedTool) && tool.active) {
+                    tool.onClick?.();  // 이전 활성화된 도구의 onClick 호출
+                }
+                return tool;
+            })
+        );
+    }, []);
+
+    const handleToolClick = useCallback((clickedTool: MapTool) => {
+        deactivatePreviousActiveTool(clickedTool);
+        updateToolActiveState(clickedTool);
+        clickedTool.onClick();
+    }, [ updateToolActiveState, deactivatePreviousActiveTool]);
+
 
     useEffect(() => {
         const labelDiv = divRef.current;
@@ -43,14 +113,9 @@ export const ObjectToolbox = () => {
 
     return options.isOpenObjectTool && (
         <div ref={divRef} id="object-toolbox">
-            <button onClick={toggleTranslation}>이동</button>
-            <button onClick={toggleRotation}>회전</button>
-            <button onClick={toggleScaling}>크기</button>
-            <button onClick={copyObject}>복사</button>
-            <button onClick={removeObject}>삭제</button>
-            <button onClick={objectAddFloor}>층+</button>
-            <button onClick={objectRemoveFloor}>층-</button>
-            <button onClick={toggleColoring}>색상</button>
+            {tools.map(tool => (
+                <ToolButton key={tool.className} tool={tool} handleClick={handleToolClick} />
+            ))}
             {options.isColoring && (
                 <input type="color" onChange={objectColoring}/>
             )}
