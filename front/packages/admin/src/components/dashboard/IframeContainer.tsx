23c9a3905312cbd2daffaf64841d3577a0f3e5be
sungjun-gaia3d <sjcho@gaia3d.com>
2024-08-06 10:56:21 +0900

    enhancement: 대시보드 다국어 처리

--- front/packages/admin/src/components/dashboard/IframeContainer.tsx
+++ front/packages/admin/src/components/dashboard/IframeContainer.tsx
@@ -1,34 +1,36 @@
 import { UrlListType } from "../../types/Dashboard";
+import {useTranslation} from "react-i18next";
 
 interface IframeContainerProps {
   srcList: UrlListType;
 }
 
 function IframeContainer({ srcList }: IframeContainerProps) {
+    const { t } = useTranslation();
   return (
     <div className="dashboard-content">
       <div className="content-01">
-        <span>Quick Facts</span>
+        <span>{t("quick-facts")}</span>
         <div className="content-wrapper">
           <iframe src={srcList.upTime} width="445" height="200" className="chart"></iframe>
           <iframe src={srcList.startTime} width="445" height="200" className="chart"></iframe>
         </div>
       </div>
       <div className="content-02">
-        <span>JVM Memory</span>
+        <span>{t("jvm-memory")}</span>
         <div className="content-wrapper">
           <iframe src={srcList.jvmTotal} width="600" height="200" className="chart"></iframe>
         </div>
       </div>
       <div className="content-03">
-        <span>JVM Misc</span>
+        <span>{t("jvm-misc")}</span>
         <div className="content-wrapper">
           <iframe src={srcList.cpuUsage} width="750" height="230" className="chart"></iframe>
           <iframe src={srcList.logEvents} width="750" height="230" className="chart"></iframe>
         </div>
       </div>
       <div className="content-04">
-        <span>I/O OverView</span>
+        <span>{t("i-o-overview")}</span>
         <div className="content-wrapper">
           <iframe src={srcList.rate} width="750" height="200" className="chart"></iframe>
           <iframe src={srcList.errors} width="750" height="200" className="chart"></iframe>
