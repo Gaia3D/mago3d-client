4a6115c0309e9c998b151e93c446a33c18d2bf46
sjcho-gaia3d <sjcho@gaia3d.com>
2024-08-09 10:41:48 +0900

    documentation: font, css 세팅

--- front/packages/user/src/Main.tsx
+++ front/packages/user/src/Main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import {RecoilEnv, RecoilRoot} from "recoil";
 import App from "./App";
 import "@/locales/i18nconfig";
+import "./assets/scss/style.scss";
 
 RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 ReactDOM.createRoot(document.getElementById("container") as HTMLElement).render(
