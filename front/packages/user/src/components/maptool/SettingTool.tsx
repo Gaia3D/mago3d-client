41e7e9965266ef5ee66ef8b4dfd04fb18c0a21d6
sjcho-gaia3d <sjcho@gaia3d.com>
2024-09-09 18:05:13 +0900

    enhancement: graphic setting 번역

--- front/packages/user/src/components/maptool/SettingTool.tsx
+++ front/packages/user/src/components/maptool/SettingTool.tsx
@@ -1,9 +1,10 @@
 import {useRecoilState} from "recoil";
 import {OptionsState} from "@/recoils/Tool.ts";
 import {useSettingTool} from "@/hooks/useMapTool/useSettingTool.ts";
+import {useTranslation} from "react-i18next";
 
 export const SettingTool = () => {
-
+    const {t} = useTranslation();
     const [options, setOptions ] = useRecoilState(OptionsState);
     const { toggleShadow, toggleSSAO, toggleEdge, toggleLighting, toggleFxaa, setShadowQuality, setResolution } = useSettingTool()
 
@@ -15,162 +16,161 @@ export const SettingTool = () => {
     }
 
     return options.isSetting && (
-        <div className="default-layer option-tool">
-            <header>
-                <h2>Graphic Setting</h2>
-                <button className="close" onClick={closeSettingTool}>
-                    <span className="minimize"></span>
-                </button>
-            </header>
-            <div className="group">
-                {/* Rendering Resolution */}
-                <div>
-                    <h3>Rendering Resolution</h3>
-                    <p>Set the rendering resolution, Higher values have a greater impact on performance.</p>
-                    {['very-high', 'high', 'mid', 'low', 'very-low'].map((level) => (
-                        <label key={level}>
-                            {level.charAt(0).toUpperCase() + level.slice(1)}
+        <>
+            <div className="pop-layer-graphic-setting">
+                <div className="pop-layer-header">
+                    <h3 className="title">{t("graphic-setting")}</h3>
+                    <button onClick={closeSettingTool} type="button" className="close-button"></button>
+                </div>
+                <div className="pop-layer-content">
+                    <div className="setting-container">
+                        <h4>{t("setting.resolution")}</h4>
+                        <span className="stxt">{t("setting.description.resolution")}</span>
+                        {['very-high', 'high', 'mid', 'low', 'very-low'].map((level) => (
+                            <label key={level}>
+                                <input
+                                    type="radio"
+                                    checked={options.renderOptions.renderQuality === level}
+                                    onChange={() => setResolution(level)}
+                                    value={level}
+                                />
+                                {t(level)}
+                            </label>
+                        ))}
+                    </div>
+                    {/* Shadow */}
+                    <div className="setting-container">
+                        <h4>{t("setting.shadow")}</h4>
+                        <span className="stxt">{t("setting.description.shadow")}</span>
+                        <label>
+                            {t("on")}
                             <input
                                 type="radio"
-                                checked={options.renderOptions.renderQuality === level}
-                                onChange={() => setResolution(level)}
-                                value={level}
+                                checked={options.renderOptions.isShadow === true}
+                                onChange={() => toggleShadow(true)}
+                                value="true"
                             />
                         </label>
-                    ))}
-                </div>
-                {/* Shadow */}
-                <div>
-                    <h3>Shadow</h3>
-                    <p>Enable/disable shadows</p>
-                    <label>
-                        On
-                        <input
-                            type="radio"
-                            checked={options.renderOptions.isShadow === true}
-                            onChange={() => toggleShadow(true)}
-                            value="true"
-                        />
-                    </label>
-                    <label>
-                        Off
-                        <input
-                            type="radio"
-                            checked={options.renderOptions.isShadow === false}
-                            onChange={() => toggleShadow(false)}
-                            value="false"
-                        />
-                    </label>
-                </div>
-                {/* Shadow Quality */}
-                <div>
-                    <h3>Shadow Quality</h3>
-                    <p>Set the shadow quality.</p>
-                    {['very-high', 'high', 'mid', 'low', 'very-low'].map((level) => (
-                        <label key={level}>
-                            {level.charAt(0).toUpperCase() + level.slice(1)}
+                        <label>
+                            {t("off")}
                             <input
                                 type="radio"
-                                checked={options.renderOptions.shadowQuality === level}
-                                onChange={() => setShadowQuality(level)}
-                                value={level}
+                                checked={options.renderOptions.isShadow === false}
+                                onChange={() => toggleShadow(false)}
+                                value="false"
                             />
                         </label>
-                    ))}
-                </div>
-                {/* Global Lighting */}
-                <div>
-                    <h3>Global Lighting</h3>
-                    <p>Visualise global illumination. Affected by time of day.</p>
-                    <label>
-                        On
-                        <input
-                            type="radio"
-                            checked={options.renderOptions.isLighting}
-                            onChange={() => toggleLighting(true)}
-                            value="true"
-                        />
-                    </label>
-                    <label>
-                        Off
-                        <input
-                            type="radio"
-                            checked={!options.renderOptions.isLighting}
-                            onChange={() => toggleLighting(false)}
-                            value="false"
-                        />
-                    </label>
-                </div>
-                {/* Anti-Aliasing (FXAA) */}
-                <div>
-                    <h3>Anti-Aliasing (FXAA)</h3>
-                    <p>Visualise anti-aliasing.</p>
-                    <label>
-                        On
-                        <input
-                            type="radio"
-                            checked={options.renderOptions.isFxaa}
-                            onChange={() => toggleFxaa(true)}
-                            value="true"
-                        />
-                    </label>
-                    <label>
-                        Off
-                        <input
-                            type="radio"
-                            checked={!options.renderOptions.isFxaa}
-                            onChange={() => toggleFxaa(false)}
-                            value="false"
-                        />
-                    </label>
-                </div>
-                {/* Edge */}
-                <div>
-                    <h3>Edge</h3>
-                    <p>Visualise edge of objects.</p>
-                    <label>
-                        On
-                        <input
-                            type="radio"
-                            checked={options.renderOptions.isEdge}
-                            onChange={() => toggleEdge(true)}
-                            value="true"
-                        />
-                    </label>
-                    <label>
-                        Off
-                        <input
-                            type="radio"
-                            checked={!options.renderOptions.isEdge}
-                            onChange={() => toggleEdge(false)}
-                            value="false"
-                        />
-                    </label>
-                </div>
-                {/* Screen Space Ambient Occlusion */}
-                <div>
-                    <h3>Screen Space Ambient Occlusion</h3>
-                    <p>Visualise detailed shading between objects.</p>
-                    <label>
-                        On
-                        <input
-                            type="radio"
-                            checked={options.renderOptions.isSSAO}
-                            onChange={() => toggleSSAO(true)}
-                            value="true"
-                        />
-                    </label>
-                    <label>
-                        Off
-                        <input
-                            type="radio"
-                            checked={!options.renderOptions.isSSAO}
-                            onChange={() => toggleSSAO(false)}
-                            value="false"
-                        />
-                    </label>
+                    </div>
+                    {/* Shadow Quality */}
+                    <div className="setting-container">
+                        <h4>{t("setting.shadow-quality")}</h4>
+                        <span className="stxt">{t("setting.description.shadow-quality")}</span>
+                        {['very-high', 'high', 'mid', 'low', 'very-low'].map((level) => (
+                            <label key={level}>
+                                {t(level)}
+                                <input
+                                    type="radio"
+                                    checked={options.renderOptions.shadowQuality === level}
+                                    onChange={() => setShadowQuality(level)}
+                                    value={level}
+                                />
+                            </label>
+                        ))}
+                    </div>
+                    {/* Global Lighting */}
+                    <div className="setting-container">
+                        <h4>{t("setting.global-lighting")}</h4>
+                        <span className="stxt">{t("setting.description.global-lighting")}</span>
+                        <label>
+                            {t("on")}
+                            <input
+                                type="radio"
+                                checked={options.renderOptions.isLighting}
+                                onChange={() => toggleLighting(true)}
+                                value="true"
+                            />
+                        </label>
+                        <label>
+                            {t("off")}
+                            <input
+                                type="radio"
+                                checked={!options.renderOptions.isLighting}
+                                onChange={() => toggleLighting(false)}
+                                value="false"
+                            />
+                        </label>
+                    </div>
+                    {/* Anti-Aliasing (FXAA) */}
+                    <div className="setting-container">
+                        <h4>{t("setting.fxaa")}</h4>
+                        <span className="stxt">{t("setting.description.fxaa")}</span>
+                        <label>
+                            {t("on")}
+                            <input
+                                type="radio"
+                                checked={options.renderOptions.isFxaa}
+                                onChange={() => toggleFxaa(true)}
+                                value="true"
+                            />
+                        </label>
+                        <label>
+                            {t("off")}
+                            <input
+                                type="radio"
+                                checked={!options.renderOptions.isFxaa}
+                                onChange={() => toggleFxaa(false)}
+                                value="false"
+                            />
+                        </label>
+                    </div>
+                    {/* Edge */}
+                    <div className="setting-container">
+                        <h4>{t("setting.edge")}</h4>
+                        <span className="stxt">{t("setting.description.edge")}</span>
+                        <label>
+                            {t("on")}
+                            <input
+                                type="radio"
+                                checked={options.renderOptions.isEdge}
+                                onChange={() => toggleEdge(true)}
+                                value="true"
+                            />
+                        </label>
+                        <label>
+                            {t("off")}
+                            <input
+                                type="radio"
+                                checked={!options.renderOptions.isEdge}
+                                onChange={() => toggleEdge(false)}
+                                value="false"
+                            />
+                        </label>
+                    </div>
+                    {/* Screen Space Ambient Occlusion */}
+                    <div className="setting-container">
+                        <h4>{t("setting.ambient")}</h4>
+                        <span className="stxt">{t("setting.description.ambient")}</span>
+                        <label>
+                            {t("on")}
+                            <input
+                                type="radio"
+                                checked={options.renderOptions.isSSAO}
+                                onChange={() => toggleSSAO(true)}
+                                value="true"
+                            />
+                        </label>
+                        <label>
+                            {t("off")}
+                            <input
+                                type="radio"
+                                checked={!options.renderOptions.isSSAO}
+                                onChange={() => toggleSSAO(false)}
+                                value="false"
+                            />
+                        </label>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 };
