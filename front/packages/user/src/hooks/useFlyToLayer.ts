885387d46ca926b3032111fb20b7778def8913c9
sjcho-gaia3d <sjcho@gaia3d.com>
2024-09-12 23:13:24 +0900

    refactor: code review 적용

--- front/packages/user/src/hooks/useFlyToLayer.ts
+++ front/packages/user/src/hooks/useFlyToLayer.ts
@@ -39,8 +39,6 @@ export const useFlyToLayer = () => {
     }, []);
 
     const flyToLayer = useCallback((asset: UserLayerAsset) => {
-
-        console.log(asset)
         const layerType = asset.type;
         const viewer = globeController.viewer;
         if (!viewer || !layerType) return;
