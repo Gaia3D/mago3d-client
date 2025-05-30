03ae19752417cae7c6ed41346ab109b917abb618
sjcho-gaia3d <sjcho@gaia3d.com>
2025-05-30 15:39:51 +0900

    feat: 공통된 코드 컴포넌트 분리

--- front/packages/admin/src/layer-style/components/fields/PointFormField.tsx
+++ front/packages/admin/src/layer-style/components/fields/PointFormField.tsx
@@ -3,6 +3,7 @@ import {EditableContextModel} from "@src/layer-style/models/EditableContextModel
 import {PreviewColumn} from "@mnd/shared/src/types/layerset/gql/graphql";
 import {FieldRow} from "@src/layer-style/components/fields/FieldRow";
 import SymbolPicker from "@src/layer-style/components/fields/picker/SymbolPicker";
+import LabelFormField from "@src/layer-style/components/fields/LabelFormField";
 
 interface PointFormFieldProps {
   context: EditableContextModel;
@@ -151,75 +152,11 @@ const PointFormField = ({context, onChange, attributes}: PointFormFieldProps) =>
         onChange={value => onChange("isLabel", value)}
       />
 
-      {context.isLabel && (
-        <div className="inner-body">
-          <FieldRow
-            id="labelAttribute"
-            label="라벨 속성"
-            type="select"
-            value={context.labelAttribute}
-            onChange={value => onChange("labelAttribute", value)}
-            options={attributes?.map(attr => ({value: attr.field, label: attr.field}))}
-          />
-
-          <FieldRow
-            id="labelFontSize"
-            label="라벨 폰트 크기"
-            type="number"
-            value={context.labelFontSize}
-            onChange={value => onChange("labelFontSize", value)}
-          />
-
-          <FieldRow
-            id="labelFillColor"
-            label="라벨 폰트 색상"
-            type="color"
-            value={context.labelFillColor}
-            onChange={value => onChange("labelFillColor", value)}
-          />
-
-          <FieldRow
-            id="labelFillOpacity"
-            label="라벨 폰트 투명도"
-            type="number"
-            min={0}
-            max={1}
-            step={0.01}
-            value={context.labelFillOpacity}
-            onChange={value => onChange("labelFillOpacity", value)}
-          />
-
-          <FieldRow
-            id="isHalo"
-            label="라벨 테두리 사용"
-            type="checkbox"
-            value={context.isHalo}
-            onChange={value => onChange("isHalo", value)}
-          />
-          {context.isHalo && (
-            <div className="inner-body">
-              <FieldRow
-                id="haloFillColor"
-                label="테두리 색상"
-                type="color"
-                value={context.haloFillColor}
-                onChange={value => onChange("haloFillColor", value)}
-              />
-
-              <FieldRow
-                id="haloFillOpacity"
-                label="테두리 투명도"
-                type="number"
-                min={0}
-                max={1}
-                step={0.01}
-                value={context.haloFillOpacity}
-                onChange={value => onChange("haloFillOpacity", value)}
-              />
-            </div>
-          )}
-        </div>
-      )}
+      <LabelFormField
+        context={context}
+        onChange={onChange}
+        attributes={attributes}
+      />
     </>
   );
 };
