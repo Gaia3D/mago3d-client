c0de45187f507c90e2b43332d912bc8d655142e6
sjcho-gaia3d <sjcho@gaia3d.com>
2025-07-29 17:16:24 +0900

    hotfix: codegen import 경로 수정

--- front/packages/admin/src/components/layerset/layer/LayerForm.tsx
+++ front/packages/admin/src/components/layerset/layer/LayerForm.tsx
@@ -1,10 +1,10 @@
-import React, {useEffect} from "react";
+import React from "react";
 import { UseFormReturn } from "react-hook-form";
-import { UpdateAssetInput } from "@src/generated/gql/layerset/graphql";
 import { getPublishStatusName } from "@src/api/Data";
 import { useTranslation } from "react-i18next";
 import {useRecoilValue} from "recoil";
 import {selectedAssetState} from "@src/layer-style/recoils/layerStyle";
+import {UpdateAssetInput} from "@mnd/shared/src/types/layerset/gql/graphql";
 
 interface LayerFormProps {
   groups: { id: string; name: string }[];
@@ -56,7 +56,7 @@ const LayerForm = ({ groups, form, onSubmit, onDelete, onCancel }: LayerFormProp
 
       <label>{t("printable")}</label>
       <label className="switch mt8">
-        <input type="checkbox" defaultChecked={asset.printable} {...register("printable" as keyof UpdateAssetInput)} />
+        <input type="checkbox" defaultChecked={asset.printable} {...register("printable")} />
         <span className="slider"></span>
       </label>
 
